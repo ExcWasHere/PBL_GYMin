cb682c5f7cc26486aba751015ae96c7422d8663a
hwollwage <hanzelputrawollwage@gmail.com>
2026-06-09 16:15:44 +0700

    add prog. comment

--- app/Http/Controllers/ProgressController.php
+++ app/Http/Controllers/ProgressController.php
@@ -9,11 +9,13 @@ use Illuminate\Support\Facades\Auth;
 
 class ProgressController extends Controller
 {
+    // get all user log
     public function index()
     {
         /** @var User $user */
 
         $user = Auth::user();
+        // get logs by date (oldest first)
         $logs = $user->progressLogs()->orderBy('log_date', 'asc')->get();
 
         return view('components.dashboard.progress', compact('logs'));
@@ -21,6 +23,7 @@ class ProgressController extends Controller
 
     public function store(Request $request)
     {
+        // validate form data
         $data = $request->validate([
             'log_date' => 'required|date',
             'weight_kg' => 'nullable|numeric|min:1|max:300',
@@ -32,6 +35,8 @@ class ProgressController extends Controller
         /** @var User $user */
 
         $user = Auth::user();
+
+        // if log already exist on this date, update it if not create new
         $user->progressLogs()->updateOrCreate(['log_date' => $data['log_date']],$data);
 
         return back()->with('success', 'progress berhasil disimpan!');
